c9ab6805df81120cf29780f59dc69460e4077c97
Esther Manrique <esther_manrique@epam.com>
2022-09-10 22:09:22 -0500

    Added app-header.component and navbar

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,8 +1,15 @@
 import 'package:angular/angular.dart';
+// import 'package:angular_router/angular_router.dart';
+import 'package:angular_app/src/components/app-header/app-header.component.dart';
+import 'package:angular_app/src/components/courses/courses_component.dart';
 
 @Component(
   selector: 'my-app',
-  template: 'app_component.html',
-  directives: [],
+  templateUrl: './app_component.html',
+  directives: [
+    // routerDirectives,
+    AppHeader,
+    CoursesComponent,
+  ],
 )
 class AppComponent {}
---
+++ lib/src/components/courses/courses_component.dart
@@ -0,0 +1,11 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'courses',
+  templateUrl: './courses_component.html',
+  directives: [
+    coreDirectives,
+  ],
+  providers: [],
+)
+class CoursesComponent {}
